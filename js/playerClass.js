22f61f991c4747a215b1458ad29e5d6dd8b836cc
VolodymyrVH <gorelovv515@gmail.com>
2025-05-06 18:20:59 +0200

    Added class for Player

---
+++ js/playerClass.js
@@ -0,0 +1,49 @@
+class Player {
+    constructor(name, hp, strength, endurance) {
+        this.name = name;
+        this.hp = hp;
+        this.strength = strength;
+        this.endurance = endurance;
+
+        this.xp = 0;
+        this.maxXp = 6;
+        this.coins = 0;
+        this.level = 0;
+        this.points = 5;
+
+        this.strengthPoint = 0;
+        this.endurancePoint = 0;
+    }
+
+    getTotalTempPoints() {
+        return this.strengthPoint + this.endurancePoint;
+    }
+
+    calculateAbilityPoints() {
+        const totalToApply = this.getTotalTempPoints();
+
+        this.strength += this.strengthPoint;
+        this.endurance += this.endurancePoint;
+        this.points -= totalToApply;
+
+        this.strengthPoint = 0;
+        this.endurancePoint = 0;
+    }
+
+    canBeApplied() {
+        return this.getTotalTempPoints() !== 0;
+    }
+
+    resetPoints() {
+        this.strengthPoint = 0;
+        this.endurancePoint = 0;
+    }
+}
+
+const playersTypes = {
+    strongman: new Player('Strongman', 20, 10, 1),
+    tank: new Player('Tank', 40, 1, 1),
+    hardyman: new Player('Hardyman', 20, 1, 10),
+};
+
+export {Player, playersTypes};
